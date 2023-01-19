347a3c38bf6e2fb833e12bc0eafa28816e0cbd8b
Sawyer020 <78552376+Sawyer020@users.noreply.github.com>
2023-01-19 16:16:57 -0500

    add comments

--- src/store/ArrStatus/reducer.ts
+++ src/store/ArrStatus/reducer.ts
@@ -5,18 +5,30 @@ import handleArr from "./index"
 //Expressions - Destructuring assignment(avoid excessive assignements)
 //...handleNum.state,
 
-//Call dispatch to run the function
+//Call dispatch to run the function 
 let reducer = (state = { ...handleArr.state }, action: { type: string, val: number }) => {
     console.log('run reducer')
     let newState = JSON.parse(JSON.stringify(state))
 
-    switch (action.type) {
-        case handleArr.sarrPush:
-            handleArr.actions[handleArr.sarrPush](newState, action)
+    //Idea: compare 'action.type' with every case, just like traverse
+    // switch (action.type) {
+    //     case handleArr.sarrPush:
+    //         handleArr.actions[handleArr.sarrPush](newState, action)
+    //         break;
+    //     default:
+    //         break;
+    // }
+
+    //[Optimize]: create an object to contain all case, called 'actionNames'
+    for (let key in handleArr.actionNames) {
+        //Using '===' to check both value and type
+        if (action.type === handleArr.actionNames[key]) {
+            handleArr.actions[handleArr.actionNames[key]](newState, action);
             break;
-        default:
-            break;
+        }
     }
+    
+
     return newState
 }
 
